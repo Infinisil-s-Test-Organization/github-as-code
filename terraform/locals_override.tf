a87ca481620c4dd02f8a9ef23e306ab93401b60e
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-03-28 21:52:37 +0100

    Fix syntax

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -1,14 +1,14 @@
 locals {
   resource_types = [
     # From https://github.com/ipdxco/github-as-code/blob/master/docs/ABOUT.md#supported-resources
-    # "github_membership"
-    # "github_repository"
-    # "github_repository_collaborator"
-    # "github_branch_protection"
-    "github_team"
-    "github_team_repository"
-    # "github_team_membership"
-    # "github_repository_file"
-    # "github_issue_label"
+    # "github_membership",
+    # "github_repository",
+    # "github_repository_collaborator",
+    # "github_branch_protection",
+    "github_team",
+    "github_team_repository",
+    # "github_team_membership",
+    # "github_repository_file",
+    # "github_issue_label",
   ]
 }

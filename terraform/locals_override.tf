699a9b02072b3da7050dfd0acfb3cf91fc61eeb1
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-03-28 21:58:43 +0100

    Also enable team
    Looks like team_membership by itself doesn't work:
    
    https://github.com/Infinisil-s-Test-Organization/github-as-code/actions/runs/14137711595/job/39613238926

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -5,7 +5,7 @@ locals {
     # "github_repository",
     # "github_repository_collaborator",
     # "github_branch_protection",
-    # "github_team",
+    "github_team",
     # "github_team_repository",
     "github_team_membership",
     # "github_repository_file",

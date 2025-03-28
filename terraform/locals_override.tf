53943b77f8001f24226c5d4e671cdb2512da72f9
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-03-28 21:50:06 +0100

    Membership as well

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -6,7 +6,7 @@ locals {
     # "github_repository_collaborator"
     # "github_branch_protection"
     "github_team"
-    # "github_team_repository"
+    "github_team_repository"
     # "github_team_membership"
     # "github_repository_file"
     # "github_issue_label"

d4bb894b1fea870a0ccd99cb2816c6aebdedd934
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-03-28 21:40:07 +0100

    Only track teams

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -1,6 +1,14 @@
 locals {
   resource_types = [
-    "github_repository",
-    "github_branch_protection"
+    # From https://github.com/ipdxco/github-as-code/blob/master/docs/ABOUT.md#supported-resources
+    # "github_membership"
+    # "github_repository"
+    # "github_repository_collaborator"
+    # "github_branch_protection"
+    "github_team"
+    # "github_team_repository"
+    # "github_team_membership"
+    # "github_repository_file"
+    # "github_issue_label"
   ]
 }
